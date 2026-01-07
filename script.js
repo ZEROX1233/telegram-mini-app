11a6d38335607c130197efc601e593dd0e101212
ZEROX1233 <pokegamer551155@gmail.com>
2026-01-07 12:23:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const tg = window.Telegram.WebApp;
+tg.expand();
+
+function buy() {
+  alert(
+    "Pay via UPI\n\nUPI ID: yourupi@bank\nAmount: ₹299\n\nPayment ke baad admin ko message karo"
+  );
+}
